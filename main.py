785b69208cfd04d3f285bc4526efba4a6a1c3da4
InColumi <max77648@gmail.com>
2024-03-27 21:14:41 +0300

    Refactor

--- main.py
+++ main.py
@@ -19,11 +19,11 @@ if __name__ == '__main__':
     print('Year data: ', year_data.shape)
     date_in = year_data['Дата и время визита']
     summer_date = year_data.loc[(date_in > '2023-05-31') & (date_in < '2023-09-01')]
-    another_date :DataFrame = year_data.loc[(date_in <= '2023-05-31') | (date_in >= '2023-09-01')]
+    another_date:DataFrame = year_data.loc[(date_in <= '2023-05-31') | (date_in >= '2023-09-01')]
     print('Summer: ', summer_date.shape)
     print('Another_date: ', another_date.shape)
     print('Summ: ', year_data.shape[0], '=' ,summer_date.shape[0] + another_date.shape[0])
-    answer : DataFrame = another_date.loc[~another_date['ФИО пациента'].isin(summer_date['ФИО пациента'])]
+    answer: DataFrame = another_date.loc[~another_date['ФИО пациента'].isin(summer_date['ФИО пациента'])]
     answer.drop(['Дата и время визита'], axis=1, inplace=True)
     grouped = answer.groupby(['ФИО пациента', 'Номер телефона']).sum()
     grouped.to_excel('answer.xlsx')
